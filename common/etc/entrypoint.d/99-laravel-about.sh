cb852cd6d4cd285d0bdb10dc2f1add2a5317d91b
Andrei Ioniță <hi@andrei.io>
2026-03-17 16:48:13 +0000

    chore: use artisan script instead of building the path every time (#9)

--- common/etc/entrypoint.d/99-laravel-about.sh
+++ common/etc/entrypoint.d/99-laravel-about.sh
@@ -3,8 +3,8 @@ script_name="laravel-about"
 
 if [ "$DISABLE_DEFAULT_CONFIG" = "false" ]; then
     # Check to see if an Artisan file exists and assume it means Laravel is configured.
-    if [ -f "$APP_BASE_DIR/artisan" ] && [ "${AUTORUN_LARAVEL_ABOUT:=true}" = "true" ]; then
-        php "$APP_BASE_DIR/artisan" about
+    if [ "${AUTORUN_LARAVEL_ABOUT:=true}" = "true" ]; then
+       artisan about
     fi
 else
     if [ "$LOG_OUTPUT_LEVEL" = "debug" ]; then
